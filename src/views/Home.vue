0568551669a0434accab7e7647c0fc5db73b53c0
Julian <julian.blazek@outlook.de>
2021-03-02 17:56:06 +0100

    Add agent

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Welcome v-if="!loggedIn" />
+    <Agent v-else/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Welcome from "@/components/Welcome.vue";
+import Agent from '../components/Agent.vue';
 
 export default {
   name: "Home",
   components: {
     Welcome,
+    Agent,
   },
   computed: {
     loggedIn() {
