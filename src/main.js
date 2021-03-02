57a466c3f2e1f02e9ee688ff822073715dcb8e6f
Julian <julian.blazek@outlook.de>
2021-03-02 17:54:33 +0100

    Add axios

--- src/main.js
+++ src/main.js
@@ -6,6 +6,17 @@ import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import { globalStore } from './store'
 
+import axios from 'axios'
+
+Vue.prototype.$http = axios.create({
+  baseURL: 'https://pda-backend.herokuapp.com',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  timeout: 10000
+})
+
 Vue.prototype.$globals = globalStore
 Vue.config.productionTip = false
 
