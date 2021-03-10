123d9a9c18fb73e19188ff89449c85aaa93672b0
Julian <julian.blazek@outlook.de>
2021-03-10 15:15:13 +0100

    Change more names

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pwa: {
+    name: "Food & Fitness Agent",
+    manifestOptions: {
+      short_name: "FFA"
+    }
+  }
 }
