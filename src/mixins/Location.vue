5c3983024078036c7617111d4e92551da7979064
Julian <julian.blazek@outlook.de>
2021-03-17 21:04:48 +0100

    Add mixins

---
+++ src/mixins/Location.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data: () => ({
+    locationRequested: false,
+    location: null,
+    locationError: null,
+  }),
+  methods: {
+    requestLocation() {
+      if (!("geolocation" in navigator)) {
+        this.errorStr = "Ortung ist für diesen Browser leider nicht verfügbar.";
+        return;
+      }
+      this.gettingLocation = true;
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          this.gettingLocation = false;
+          this.location = pos;
+        },
+        (err) => {
+          this.gettingLocation = false;
+          this.errorStr = err.message;
+        }
+      );
+    },
+  },
+};
+</script>
---
+++ src/mixins/api/Calendar.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  methods: {
+    async getEvents(day) {
+      let calendarResult = await this.$http.get(`calendar/${day}`, {
+        headers: {
+          Authorization: this.$globals.credentials,
+        },
+      });
+      let calendarEvents = calendarResult.data.data.map((i) => {
+        i["name"] = i["title"];
+        i["start"] = new Date(i["start"]);
+        i["end"] = new Date(i["end"]);
+        i["timed"] = true;
+        return i;
+      });
+      return calendarEvents;
+    },
+    async getFreetime() {
+      
+    }
+  },
+};
+</script>
