336facb782322eeaea46d991236c66bd8036a8a8
Julian <julian.blazek@outlook.de>
2021-04-06 10:39:52 +0200

    Add more Notifications

--- src/components/NotificationManager.vue
+++ src/components/NotificationManager.vue
@@ -28,6 +28,25 @@ export default {
         icon: "/img/morning.png",
         lastHandled: null,
       },
+      {
+        hour: 11,
+        minute: 30,
+        message: "Guten Mittag!\nVergesse deine Mittagspause nicht! 😊",
+        icon: "/img/eat.png",
+        lastHandled: null
+      },
+      {
+        hour: 16,
+        minute: 30,
+        message: "Lust auf ein bisschen Sport?\nSchau doch mal in deine Sport Routine!",
+        icon: "/img/test.png"
+      },
+      {
+        hour: 21,
+        minute: 0,
+        message: "Guten Abend!\nGehst du bald ins Bett? Dann werfe doch nochmal einen Blick in deine Gute Nacht Routine! 🌃",
+        icon: "/img/sleep_cloud.png"
+      }
     ],
   }),
   async created() {
@@ -52,7 +71,7 @@ export default {
         if (
           curDate.valueOf() - notDate.valueOf() < 5 * 60 * 1000 &&
           curDate.valueOf() - notDate.valueOf() >= 0 &&
-          (notification.lastHandled == null ||
+          (!!notification.lastHandled ||
             curDate.valueOf() - notification.lastHandled > 60 * 60 * 1000)
         ) {
           let notify = new Notification("FFA", {
