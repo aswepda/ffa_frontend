19fb1756945795ed96bd1e999ab712e4c7228c37
Julian <julian.blazek@outlook.de>
2021-04-06 10:39:24 +0200

    Add Routine Recommendation

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -2,55 +2,16 @@
   <div class="agenttest">
     <chat ref="chat" class="mt-3" />
     <transition name="fade" mode="out-in">
-      <div class="actions" v-if="!morningRoutine" key="actions">
-        <v-row justify="start" align="stretch">
-          <v-col class="col-md-6">
-            <Action
-              imageURL="/img/test.png"
-              color="#245535"
-              :featured="isFeatured('night')"
-              title="Sport 🏀"
-              >Lust auf ein bisschen Sport? :)</Action
-            >
-          </v-col>
-          <v-col class="col-md-6">
-            <Action
-              imageURL="/img/sleep_cloud.png"
-              color="#350A67"
-              title="Gute Nacht 🌃"
-              :featured="isFeatured('night')"
-              >Gute Nacht 🌃</Action
-            >
-          </v-col>
-        </v-row>
-        <v-row justify="start" class="mb-4" align="stretch">
-          <v-col class="col-md-6">
-            <Action
-              imageURL="/img/morning.png"
-              color="#F47458"
-              title="Guten Morgen ☕"
-              :featured="isFeatured('morning')"
-              @clicked="morningRoutine = true"
-            >
-              Guten Morgen! Deine Routine für den Start in den Tag. :)
-            </Action>
-          </v-col>
-          <v-col class="col-md-6">
-            <Action
-              imageURL="/img/eat.png"
-              color="#365DA4"
-              title="Essen 🥗"
-              :featured="isFeatured('eat')"
-            >
-              Frühstück, Mittagessen oder Abendessen?<br />Deine Routine für
-              jeden Hunger.
-            </Action>
+      <div class="actions" v-if="!routine" key="actions">
+        <v-row justify="start">
+          <v-col cols="12" sm="6" v-for="action in sortedActions" :key="action.key">
+            <Action :imageURL="action.imageURL" :color="action.color" :title="action.title" :featured="isFeatured(action.key)" @clicked="routine = action.key">{{action.content}}</Action>
           </v-col>
         </v-row>
       </div>
       <v-row v-else key="routine">
         <v-col>
-          <good-morning @closed="morningRoutine = false" @data="addMessage" />
+          <component :is="currentRoutine" @closed="routine = ''" @data="addMessage" />
         </v-col>
       </v-row>
     </transition>
@@ -60,16 +21,17 @@
 <script>
 import Chat from "../components/Chat.vue";
 import GoodMorning from "../components/usecases/GoodMorning.vue";
+import Food from "../components/usecases/Food.vue"
 import Action from "../components/usecases/ActionCard.vue";
 export default {
-  components: { Chat, GoodMorning, Action },
+  components: { Chat, GoodMorning, Action, Food },
   data: () => ({
     actions: [
       {
         imageURL: "/img/test.png",
         color: "#245535",
         title: "Sport 🏀",
-        key: "night",
+        key: "sport",
         content: "Lust auf ein bisschen Sport? :)",
       },
       {
@@ -92,10 +54,11 @@ export default {
         title: "Essen 🥗",
         key: "eat",
         content:
-          "Frühstück, Mittagessen oder Abendessen?<br />Deine Routine fürjeden Hunger.",
+          `Frühstück, Mittagessen oder Abendessen?
+Deine Routine für jeden Hunger.`,
       },
     ],
-    morningRoutine: false,
+    routine: ''
   }),
   methods: {
     getPageHeight() {
@@ -120,7 +83,9 @@ export default {
     },
   },
   activated() {
+    if(this.$refs.chat.messages.length > 0) {
     this.scrollToBottom();
+    }
   },
   computed: {
     featured() {
@@ -130,10 +95,23 @@ export default {
       if (curHours >= 11 && curHours <= 14) featured.push("eat");
       if (curHours >= 14 && curHours <= 19) featured.push("sport");
       if (curHours >= 17 && curHours <= 20) featured.push("eat");
-      if ((curHours > 20) | (curHours <= 3)) featured.push("night");
+      if ((curHours > 20) || (curHours <= 3)) featured.push("night");
       console.log(curHours);
       return featured;
     },
+    sortedActions() {
+      return [...this.actions].sort((a, b) => this.isFeatured(b.key) - this.isFeatured(a.key));
+    },
+    currentRoutine() {
+      switch(this.routine) {
+        case "morning":
+          return GoodMorning;
+        case "eat":
+          return Food;
+        default:
+          return "";
+      }
+    }
   },
 };
 </script>
@@ -145,5 +123,11 @@ export default {
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
+}
+.actions .v-card__title {
+  word-break: normal;
+}
+.actions .v-card__subtitle {
+  white-space: pre-line;
 }
 </style>
