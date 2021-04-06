da2becbd22588b87953f167ded66bacb77019c39
Julian <julian.blazek@outlook.de>
2021-04-06 11:57:32 +0200

    Add direction->text parsing

--- src/mixins/api/Directions.vue
+++ src/mixins/api/Directions.vue
@@ -1,17 +1,31 @@
 <script>
-import GPSLocation from '../Location';
+import GPSLocation from "../Location";
 export default {
   methods: {
     async getDirection(dest, mode, lat = null, lon = null) {
-      if(!lat || !lon) {
-          var location = await this.requestLocation();
-          lat = location.coords.latitude;
-          lon = location.coords.longitude;
+      if (!lat || !lon) {
+        var location = await this.requestLocation();
+        lat = location.coords.latitude;
+        lon = location.coords.longitude;
       }
-      let dirData = await this.$http.get(`directions/${lat},${lon}/${mode}/${dest}`);
+      let dirData = await this.$http.get(
+        `directions/${lat},${lon}/${mode}/${dest}`
+      );
       return dirData.data.data;
     },
+    parseDirectionText(direction) {
+      switch(direction) {
+        case "driving":
+          return "mit dem Auto";
+        case "walking":
+          return "zu Fuß";
+        case "transit":
+          return "mit ÖPNV";
+        case "bicycling":
+          return "mit dem Fahrrad"
+      }
+    },
   },
-  mixins: [GPSLocation]
+  mixins: [GPSLocation],
 };
 </script>
