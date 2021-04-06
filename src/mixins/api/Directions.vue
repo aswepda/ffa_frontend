dbc0c99afe7d257f9899d5121148300050ceade0
Julian <julian.blazek@outlook.de>
2021-04-06 11:35:58 +0200

    Add Directions Mixin

---
+++ src/mixins/api/Directions.vue
@@ -0,0 +1,17 @@
+<script>
+import GPSLocation from '../Location';
+export default {
+  methods: {
+    async getDirection(dest, mode, lat = null, lon = null) {
+      if(!lat || !lon) {
+          var location = await this.requestLocation();
+          lat = location.coords.latitude;
+          lon = location.coords.longitude;
+      }
+      let dirData = await this.$http.get(`directions/${lat},${lon}/${mode}/${dest}`);
+      return dirData.data.data;
+    },
+  },
+  mixins: [GPSLocation]
+};
+</script>
