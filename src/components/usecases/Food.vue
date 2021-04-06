05ede3fe2dc002a700f1464f3721fca93f94b207
Julian <julian.blazek@outlook.de>
2021-04-06 10:39:59 +0200

    Add Food Usecase

---
+++ src/components/usecases/Food.vue
@@ -0,0 +1,135 @@
+<template>
+  <v-card color="#365DA4" dark height="100%">
+    <v-card-title
+      ><span>{{ mealText }}! {{ mealEmoji }}</span
+      ><v-spacer></v-spacer
+      ><v-btn icon @click="close" class="ml-auto"
+        ><v-icon>mdi-close</v-icon></v-btn
+      ></v-card-title
+    >
+    <v-card-text
+      >Hey {{ firstName }}! Ich bin dir beim Planen deiner nächsten Mahlzeit
+      gerne behilflich! 😊</v-card-text
+    >
+    <v-card-actions>
+      <div class="d-flex flex-wrap">
+        <v-btn class="ml-2 mb-1" outlined rounded small @click="nextBreak">
+          <v-icon left small>mdi-clock</v-icon>Wann kann ich Pause machen?
+        </v-btn>
+        <v-btn class="ml-2 mb-1" outlined rounded small @click="weather"
+          ><v-icon left small>mdi-weather-partly-cloudy</v-icon>Wie wird denn
+          das Wetter?
+        </v-btn>
+        <v-btn class="ml-2 mb-1" outlined rounded small @click="calendar"
+          ><v-icon left small>mdi-calendar</v-icon>Was steht an?
+        </v-btn>
+        <v-btn class="ml-2 mb-1" outlined rounded small @click="spotifyMorning">
+          <v-icon left small>mdi-spotify</v-icon>Etwas Morgenmusik bitte!
+        </v-btn>
+      </div>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import CalendarVue from "../../mixins/api/Calendar.vue";
+export default {
+  methods: {
+    close() {
+      this.$emit("closed");
+    },
+    async nextBreak() {
+      this.$emit("data", {
+        type: "message",
+        own: true,
+        text: "Wann hätte ich Zeit für meine Pause?",
+      });
+      if (!this.$globals.credentials) {
+        this.$emit("data", {
+          type: "message",
+          own: false,
+          text:
+            "Du scheinst nicht mit Google angemeldet zu sein! Damit ich deine Kalendereinträge analysieren kann musst du dich mit Google anmelden.",
+        });
+        return;
+      }
+      this.$emit("data", {
+        type: "message",
+        own: false,
+        text: "Ich schaue kurz für dich nach! 🔎📅",
+      });
+      try {
+        let freeTime = await this.getFreetime();
+        if (!freeTime) {
+          this.$emit("data", {
+            type: "message",
+            own: false,
+            text:
+              "Du hast die nächsten 4 Stunden frei! Genieße deine Pause. 🌻",
+          });
+        } else {
+          console.log(freeTime);
+          this.$emit("data", {
+            type: "message",
+            own: false,
+            text: `Du hast zwischen ${freeTime.start
+              .toLocaleTimeString(window.navigator.language)
+              .substr(0, 5)} und ${freeTime.ende
+              .toLocaleTimeString(window.navigator.language)
+              .substr(0, 5)} Uhr ${
+              freeTime.Dauer
+            } Minuten Zeit für deine nächste Pause.`,
+            speak: true,
+          });
+          this.$emit("data", {
+            type: "calendar",
+            own: false,
+            events: [{
+              timed: true,
+              name: "Pause",
+              start: freeTime.start,
+              end: freeTime.ende
+            }],
+            openOnClick: false
+          })
+        }
+      } catch (ex) {
+        console.error(ex);
+        this.$emit("data", {
+          type: "message",
+          own: false,
+          text:
+            "Etwas ist schiefgelaufen.. Bitte versuche deinen Google-Account nochmal ab- und anzumelden.",
+        });
+      }
+    },
+  },
+  computed: {
+    mealText() {
+      let curHours = new Date().getHours();
+      if (curHours >= 4 && curHours <= 10) return "Frühstück";
+      if (curHours >= 11 && curHours <= 16) return "Mittagsessen";
+      if (curHours >= 17 && curHours <= 22) return "Abendessen";
+      return "Mitternachtssnack";
+    },
+    mealEmoji() {
+      switch (this.mealText) {
+        case "Frühstück":
+          return "🥣";
+        case "Mittagsessen":
+          return "🥗";
+        case "Abendessen":
+          return "🍝";
+        case "Mitternachtssnack":
+          return "😉";
+        default:
+          return "";
+      }
+    },
+  },
+  mixins: [CalendarVue],
+};
+</script>
+
+<style>
+</style>
