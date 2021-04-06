10f99c12527d77ba9283af0ee142506272b2f0f5
Julian <julian.blazek@outlook.de>
2021-04-06 16:48:20 +0200

    Add speech setting

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -2,16 +2,45 @@
   <div class="settings">
     <h1>Einstellungen</h1>
     <v-text-field v-model="settings.name" label="Name (überschreibt durch social login erhaltene Namen)" placeholder="-" type="text"></v-text-field>
+    <v-text-field v-model="settings.workplace" label="Arbeitsplatz" placeholder="DHBW Stuttgart" type="text"></v-text-field>
+    <v-select v-model="settings.directionMode" :items="transitModes" label="Hauptverkehrsmittel"></v-select>
+    <v-switch label="Sprachausgabe" v-model="settings.speechEnabled"></v-switch>
+    <v-btn :color="saved ? 'primary' : null" :dark="saved" @click="save"><v-icon left>mdi-content-save</v-icon>{{savedText}}</v-btn>
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    settings: {}
+    saved: false,
+    settings: {},
+    transitModes: ['driving', 'walking', 'bicycling', 'transit']
   }),
   created() {
     this.settings = {...this.$globals.settings}
+    if (!('speechEnabled' in this.settings)) {
+      this.settings.speechEnabled = true;
+    }
+  },
+  computed: {
+    savedText() {
+      if(this.saved) {
+        return "Gespeichert!"
+      } else {
+        return "Speichern"
+      }
+    },
+  },
+  methods: {
+    save() {
+      for (let key of Object.keys(this.settings)) {
+        this.$globals.setSetting(key, this.settings[key])
+      }
+      this.saved = true;
+      setTimeout(() => {
+        this.saved = false;
+      }, 5000);
+    }
   }
 }
 </script>
