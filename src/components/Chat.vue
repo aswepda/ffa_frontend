7467d3eeb9fc1e1ea9313f8aa620244895f8e2fa
Julian <julian.blazek@outlook.de>
2021-04-11 12:40:23 +0200

    Update Landingpage

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -22,131 +22,11 @@ const Speech = () => import(/* webpackChunkName: "speech" */"./content/Speech.vu
 const Weather = () => import(/* webpackChunkName: "weather" */"./content/Weather.vue");
 
 export default {
+  props: {
+    defaultMessages: Array
+  },
   data: () => ({
-    messages: [
-      /*
-      {
-        type: "message",
-        own: true,
-        id: 1,
-        text: "Zeige mir einen interessanten Ort!",
-      },
-      {
-        type: "message",
-        own: false,
-        id: 2,
-        text: "Das könnte dir vielleicht gefallen! 👀",
-      },
-      {
-        type: "speech",
-        own: false,
-        id: 2,
-        text: "Das könnte dir vielleicht gefallen!",
-      },
-      {
-        type: "location",
-        own: false,
-        id: 3,
-        lat: "48.784708",
-        lon: "9.174249",
-        name: "DHBW Stuttgart",
-        url: "https://dhbw-stuttgart.de",
-        text: `Die Duale Hochschule Baden-Württemberg ist seit dem 1. März 2009 eine
-          staatliche duale Hochschule in Baden-Württemberg, die duale
-          Studiengänge anbietet. Sie ist, unter Einbeziehung aller
-          Studienakademien, die nach Studierendenzahl größte Hochschule des
-          Landes und, wie die meisten Hochschulen in Baden-Württemberg, eine
-          Körperschaft des öffentlichen Rechts.`,
-      },
-      {
-        type: "message",
-        own: true,
-        id: 4,
-        text: "Hast du ein Lied für mich?",
-      },
-      {
-        type: "message",
-        own: false,
-        id: 5,
-        text: "Na klar! 🎶",
-      },
-      {
-        type: "spotify",
-        own: false,
-        id: 6,
-        uri: "spotify:track:4oYzwOTWpP3JTyKrTHJtqw",
-        title: "Awaken - Big Wild",
-        text: `Dieses Lied könnte dir vielleicht gefallen!`,
-      },
-      { type: "message", own: true, id: 7, text: "Was steht heute an?" },
-      {
-        type: "message",
-        own: false,
-        id: 8,
-        text: "Ich habe die folgenden Termine in deinem Kalender gefunden! 📅",
-      },
-      {
-        type: "speech",
-        own: false,
-        id: 12,
-        text:
-          "Ich habe die folgenden Termine in deinem Kalender gefunden! Heute hast du nicht so viel zu tun.",
-        autoplay: true,
-      },
-      {
-        type: "calendar",
-        own: false,
-        id: 9,
-        events: [
-          {
-            name: "Zahnarzt",
-            start: new Date().toISOString().slice(0, 10) + " 09:00",
-            end: new Date().toISOString().slice(0, 10) + " 10:30",
-            link: "https://google.com",
-          },
-          {
-            name: "Friseurbesuch",
-            start: new Date().toISOString().slice(0, 10) + " 14:00",
-            end: new Date().toISOString().slice(0, 10) + " 14:30",
-          },
-          {
-            name: "Steuererklärung abgeben",
-            start: new Date().toISOString().slice(0, 10) + " 17:00",
-            end: new Date().toISOString().slice(0, 10) + " 18:00",
-          },
-        ],
-      },
-      {
-        type: "message",
-        own: true,
-        id: 10,
-        text: "Wie ist denn das Wetter so?",
-      },
-      {
-        type: "message",
-        own: false,
-        id: 11,
-        text: "In Stuttgart ist es momentan Regnerisch bei etwa 12°C.",
-      },
-      {
-        type: "speech",
-        own: false,
-        id: 12,
-        text: "In Stuttgart ist es momentan regnerisch bei etwa 12°C.",
-        autoplay: false,
-      },
-      {
-        type: "weather",
-        own: false,
-        id: 13,
-        city: "Stuttgart",
-        icon: "10d",
-        temperature: 12,
-        windspeed: 5,
-        humidity: 13,
-        condition: "Regnerisch",
-      },*/
-    ],
+    messages: [],
   }),
   methods: {
     getComponent(message) {
@@ -245,6 +125,11 @@ export default {
       }
     },
   },
+  mounted() {
+    for(let message of this.defaultMessages) {
+      this.messages.push(message);
+    }
+  }
 };
 </script>
 
